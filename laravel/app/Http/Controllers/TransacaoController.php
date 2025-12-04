cd210ddd19ec8e21076de3950961dc6a3de0d4c4
João Mesquita <joaobmesquita1@gmail.com>
2025-12-04 15:52:35 -0200

    create TransacaoController

---
+++ laravel/app/Http/Controllers/TransacaoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\TransacaoService;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TransacaoController extends Controller
+{
+    private $transacaoService;
+
+    public function __construct()
+    {
+        $this->transacaoService = new TransacaoService(Auth::user()->id);
+    }
+
+    //my
+    public function show(){
+        return $this->transacaoService->getAll();
+    }
+}
--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TransacaoController;
 use App\Models\Transacao;
 use App\Services\TransacaoService;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    (new TransacaoService(userId: Auth::user()->id))->inserir(valor: 1000, tipo: 'receita', categoria: 'Prestação de Serviços', descricao: 'Alguma coisa importante', data: now());
-    return Transacao::with('user')->get();
-    //return view('welcome');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
@@ -17,6 +16,10 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    //transaction
+    Route::get('/transacoes', [TransacaoController::class, 'show'])->name('transacoes');
+
+    //profile
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
