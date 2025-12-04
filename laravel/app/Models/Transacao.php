27c8e27c3390142fe563494a6d59f93ea7cbcc8d
João Mesquita <joaobmesquita1@gmail.com>
2025-12-04 09:56:46 -0200

    estabelecendo relacionamento entre as models and create TransacaoŜervice and method getAll

--- laravel/app/Models/Transacao.php
+++ laravel/app/Models/Transacao.php
@@ -6,9 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transacao extends Model
 {
+    protected $table = 'transacoes';
     protected $fillable = [
         'descricao', 'categoria', 'valor',
         'tipo', 'usuario_id', 'data'
     ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'usuario_id');
+    }
 
 }
