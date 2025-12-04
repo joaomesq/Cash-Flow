b1145a56dbdf6a63a986150f8535710df3ddb490
João Mesquita <joaobmesquita1@gmail.com>
2025-12-04 15:42:19 -0200

    create method for insert transaction in database

--- laravel/app/Services/TransacaoService.php
+++ laravel/app/Services/TransacaoService.php
@@ -14,4 +14,22 @@ class TransacaoService{
     public function getAll(){
         return Transacao::where('usuario_id', $this->idUser)->paginate(10);
     }
+
+    public function inserir(float $valor, string $tipo = 'receita', string $categoria, string $descricao, string $data){
+        //validar os campos
+        if(empty($valor) || empty($categoria) || empty($categoria) || empty($data)):
+            return false;
+        endif;
+
+        //checar tipos
+        $tipos = ['receita', 'despesa'];
+        if(!in_array($tipo, $tipos)):
+            return false;
+        endif;
+
+        return  Transacao::create([
+            'valor'=> $valor, 'categoria'=> $categoria, 'tipo'=> $tipo,
+            'usuario_id'=> $this->idUser, 'descricao'=> $descricao, 'data'=> $data
+        ]);
+    }
 }
