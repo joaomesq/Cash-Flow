6ea66f5cf60dc21cb7697f628761c205299c0ff3
João Mesquita <joaobmesquita1@gmail.com>
2025-12-06 21:03:56 -0200

    create component livewire for register transacao

---
+++ laravel/app/Livewire/Transacao/RegisterTransacao.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Transacao;
+
+use Livewire\Component;
+
+class RegisterTransacao extends Component
+{
+    public function render()
+    {
+        return view('livewire.transacao.register-transacao');
+    }
+}
--- laravel/resources/views/transacao/register.blade.php
+++ laravel/resources/views/transacao/register.blade.php
@@ -1,3 +1,5 @@
 <x-app-layout>
-    
+    <section class="main">
+        <livewire:transacao.register-transacao />
+    </section>
 </x-app-layout>
