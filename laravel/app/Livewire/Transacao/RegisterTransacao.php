5881b7d476c23f45ca42f5602d291ca32aba5b1d
João Mesquita <joaobmesquita1@gmail.com>
2025-12-09 22:34:41 -0200

    montagem da estrutura html da view livewire para registrar transação

--- laravel/app/Livewire/Transacao/RegisterTransacao.php
+++ laravel/app/Livewire/Transacao/RegisterTransacao.php
@@ -2,12 +2,25 @@
 
 namespace App\Livewire\Transacao;
 
+use App\Services\TransacaoService;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class RegisterTransacao extends Component
 {
+    public $valor = 0.00;
+    public $tipo;
+    public $descricao;
+    public $categoria;
+    public $data;
+    private $transacaoService;
+
     public function render()
     {
         return view('livewire.transacao.register-transacao');
+    }
+
+    public function salvar(){
+        (new TransacaoService(userId: Auth::user()->id))->inserir(valor: $this->valor, tipo: $this->tipo, categoria: $this->categoria, descricao: $this->descricao, data: $this->data);
     }
 }
--- laravel/resources/views/livewire/transacao/register-transacao.blade.php
+++ laravel/resources/views/livewire/transacao/register-transacao.blade.php
@@ -1,3 +1,52 @@
-<div>
-    <h1>Hello World</h1>
-</div>
+<section class="insert-transacao bg-white p-4">
+    <div>
+        <form wire:submit.prevent="salvar">
+            <fieldset>
+                <div>
+                    <label>Descricao</label>
+                    <input type="text" name="input-descricao" wire:model="descricao" placeholder="Descricação da transação" required>
+                </div>
+
+                <div>
+                    <label>Categoria</label>
+                    <select wire:model="categoria" name="select-categoria">
+                        <option value="venda">Venda</option>
+                        <option value="compra">Compra</option>
+                        <option value="recebimento">Recebimento</option>
+                        <option value="pagamento">Pagamento</option>
+                        <option value="investimento">Investimento</option>
+                        <option value="prestação de serviço">Prestação de Serviço</option>
+                        <option value="despesas fixas">Despesas Fixas</option>
+                        <option value="despesas variáveis">Despesas Variáveis</option>
+                        <option value="gastos não essenciais">Gastos não essenciais</option>
+                        <option value="outros">Outros</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label>Data</label>
+                    <input type="date" name="input-data" wire:model="data" required>
+                </div>
+
+                <div>
+                    <label>Valor</label>
+                    <input type="text" name="input-valor" wire:model="valor" value="0" required>
+                </div>
+
+                <div>
+                    <p>
+                        <input type="radio" name="input-tipo" wire:model="tipo" value="entrada" checked><label>Entrada</label>
+                    </p>
+
+                    <p>
+                        <input type="radio" name="input-tipo" wire:model="tipo" value="saida"><label>Saída</label>
+                    </p>
+                </div>
+            </fieldset>
+
+            <div>
+                <button type="submit">Inserir</button>
+            </div>
+        </form>
+    </div>
+</section>
