e125de9345425299b8be746b6fe1846e8a9b53d4
João Mesquita <joaobmesquita1@gmail.com>
2025-12-10 20:48:58 -0200

    create mount in RegistrarTransacao

--- laravel/app/Livewire/Transacao/RegisterTransacao.php
+++ laravel/app/Livewire/Transacao/RegisterTransacao.php
@@ -8,12 +8,17 @@ use Livewire\Component;
 
 class RegisterTransacao extends Component
 {
-    public $valor = 0.00;
+    public $valor;
     public $tipo;
     public $descricao;
     public $categoria;
     public $data;
     private $transacaoService;
+
+    public function mount(){
+        $this->transacaoService = new TransacaoService(userId: Auth::user()->id);
+        $this->valor = 0.00;
+    }
 
     public function render()
     {
@@ -21,6 +26,11 @@ class RegisterTransacao extends Component
     }
 
     public function salvar(){
-        (new TransacaoService(userId: Auth::user()->id))->inserir(valor: $this->valor, tipo: $this->tipo, categoria: $this->categoria, descricao: $this->descricao, data: $this->data);
+        if($this->valor <= 0):
+            return;
+        endif;
+
+        $this->transacaoService->inserir(valor: $this->valor, tipo: $this->tipo, categoria: $this->categoria, descricao: $this->descricao, data: $this->data);
+        $this->reset(['valor', 'descricao', 'tipo', 'categoria', 'data']);
     }
 }
