3bfc2f21555e459ed0ba7f6cdacf23f32c3c4b67
João Mesquita <joaobmesquita1@gmail.com>
2025-12-19 20:13:57 -0200

    criando componente para gerar lista mensal das transações

---
+++ laravel/app/Livewire/Transacao/ListaMensal.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Transacao;
+
+use Livewire\Component;
+
+class ListaMensal extends Component
+{
+    public function render()
+    {
+        return view('livewire.transacao.lista-mensal');
+    }
+}
