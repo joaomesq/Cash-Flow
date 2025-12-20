1c607331cf124aabfb785cbf1fcdd273f63f9257
João Mesquita <joaobmesquita1@gmail.com>
2025-12-20 19:25:05 -0200

    Implementação do método buscar - Transações

--- laravel/app/Livewire/Transacao/Listar.php
+++ laravel/app/Livewire/Transacao/Listar.php
@@ -10,10 +10,21 @@ use Livewire\WithPagination;
 class Listar extends Component
 {
     use WithPagination;
+
+    public $descricao;
+    public $categori;
+    public $valor;
+    public $tipo;
+    public $data;
+
+    public function UpdateDescricao(){
+        $this->resetPage();
+    }
     
     public function render()
     {
-        $transacoes = (new TransacaoService(userId: Auth::user()->id))->getAll();
+        $this->descricao = 'pass';
+        $transacoes = (new TransacaoService(userId: Auth::user()->id))->buscar(descricao: $this->descricao);
         return view('livewire.transacao.listar', compact('transacoes'));
     }
 }
