35dffeb74dc426e9379cda4a4c249a8aa814b6fb
João Mesquita <joaobmesquita1@gmail.com>
2025-12-20 19:25:05 -0200

    Implementação do método buscar - Transações

--- laravel/app/Services/TransacaoService.php
+++ laravel/app/Services/TransacaoService.php
@@ -21,7 +21,7 @@ class TransacaoService{
                         ->orderBy('created_at', 'desc')->limit(15)->get();
     }
 
-    public function inserir(float $valor, string $tipo = 'receita', string $categoria, string $descricao, string $data){
+    public function inserir(float $valor, string $tipo = 'receita', string $categoria, string $descricao, $data){
         //validar os campos
         if(empty($valor) || empty($categoria) || empty($categoria) || empty($data)):
             return false;
@@ -37,6 +37,17 @@ class TransacaoService{
             'valor'=> $valor, 'categoria'=> $categoria, 'tipo'=> $tipo,
             'usuario_id'=> $this->idUser, 'descricao'=> $descricao, 'data'=> $data
         ]);
+    }
+
+    public function buscar(string|null $descricao = null, string|null $categoria = null, float|null $valor = null, string|null $tipo = null, $dataInicio = null, $dataFim = null){
+        return Transacao::query()->when($descricao, fn($q)=> $q->where('descricao', 'LIKE', "{$descricao}%"))
+                            ->when($categoria, fn($q)=> $q->where('categoria', 'LIKE', "{$categoria}%"))
+                            ->when($tipo, fn($q)=> $q->where('tipo', $tipo))
+                            ->when($valor, fn($q)=> $q->where($valor, 'LIKE', "{$valor}%"))
+                            ->when($dataInicio, fn($q)=> $q->whereDate('data', '>=', $dataInicio))
+                            ->when($dataFim, fn($q)=> $q->whereDate('data', '<=', $dataFim))
+                            ->where('usuario_id', $this->idUser)->orderBy('data', 'desc')->orderBy('created_at', 'desc')
+                            ->paginate(15);
     }
 
     /**
