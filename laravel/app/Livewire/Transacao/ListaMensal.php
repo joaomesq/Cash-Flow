bb015d2064d8d0fcb1ed2739ae93680ef44f983a
João Mesquita <joaobmesquita1@gmail.com>
2025-12-20 20:58:33 -0200

    Melhorando a atualização da listagem mensal

--- laravel/app/Livewire/Transacao/ListaMensal.php
+++ laravel/app/Livewire/Transacao/ListaMensal.php
@@ -6,29 +6,38 @@ use App\Services\TransacaoService;
 use Illuminate\Support\Facades\Auth;
 use Livewire\Attributes\On;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 use function Symfony\Component\Clock\now;
 
 class ListaMensal extends Component
 {
-    public $transacoes;
+    use WithPagination;
+
+    public $ano;
+    public $mes;
+
+    public function mount(){
+        $this->ano = now()->format('Y');
+        $this->mes = now()->format('m');
+    }
 
     public function render()
     {
-        return view('livewire.transacao.lista-mensal');
-    }
-
-    public function mount(){
-        $this->transacoes = $this->getTransacaoService()->getMensal(ano: now()->format('Y'), mes: now()->format('m'));
+        $transacoes = (new TransacaoService(userId: Auth::user()->id))->getMensal(ano: $this->ano, mes: $this->mes);
+        return view('livewire.transacao.lista-mensal', compact('transacoes'));
     }
 
     #[On('transacao-criada')]
     #[On('alterar-data')]
     public function atualizarLista(int $ano, int $mes){
-        $this->transacoes = $this->getTransacaoService()->getMensal(ano: $ano, mes: $mes);
-    }
+        if($ano === $this->ano && $mes === $this->mes){
+            $this->resetPage();
+        }else{
+            $this->ano = $ano;
+            $this->mes = $mes;
+            $this->resetPage();
+        }
 
-    private function getTransacaoService(){
-        return new TransacaoService(userId: Auth::user()->id);
     }
 }
