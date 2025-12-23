c293e708e970d6e7f2f788598b82bc05ba4b8428
João Mesquita <joaobmesquita1@gmail.com>
2025-12-23 19:58:55 -0200

    create livewire component ultimas-transacoes

---
+++ laravel/app/Livewire/Transacao/UltimasTransacoes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Transacao;
+
+use Livewire\Component;
+
+class UltimasTransacoes extends Component
+{
+    public function render()
+    {
+        return view('livewire.transacao.ultimas-transacoes');
+    }
+}
---
+++ laravel/resources/views/livewire/transacao/ultimas-transacoes.blade.php
@@ -0,0 +1,3 @@
+<div>
+    {{ "Algumas pessoas" }}
+</div>
