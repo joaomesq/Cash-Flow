4a23b322e282351e6196540092395e989ee7bc4b
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-09 20:52:09 -0300

    defindo o cálculo dos valores referentes a receita, desepesa, saldo

--- laravel/resources/views/dashboard.blade.php
+++ laravel/resources/views/dashboard.blade.php
@@ -7,10 +7,17 @@
 
     <section class="main">
         <div class="py-6 bg-white">
+            <h2>Totais gerados</h2>
+            <livewire:valores.totais />
+        </div>    
+
+        <div class="py-6 bg-white">
+            <h2>Últimas transações</h2>
             <livewire:transacao.ultimas-transacoes />
         </div>
         
         <div class="py-6 bg-white">
+            <h2>Resumo de gasto por descrição</h2>
             <livewire:transacao.resumo-transacoes />
         </div>
     </section>
