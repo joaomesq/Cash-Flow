ad16ab436226449a5a6275a91d572418fea5761b
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-09 19:51:38 -0300

    create component para calcular os valores totais, está contido em diretório que deve agrupar o calculo dos valores

---
+++ laravel/app/Livewire/Valores/Totais.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Valores;
+
+use Livewire\Component;
+
+class Totais extends Component
+{
+    public function render()
+    {
+        return view('livewire.valores.totais');
+    }
+}
