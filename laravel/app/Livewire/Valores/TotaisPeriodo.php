e335b2d7027d04d039c519c33e511769b62cf447
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-15 19:19:41 -0300

    Definição dos atributos e métodos essenciais para TotaisPeriodo

--- laravel/app/Livewire/Valores/TotaisPeriodo.php
+++ laravel/app/Livewire/Valores/TotaisPeriodo.php
@@ -2,12 +2,56 @@
 
 namespace App\Livewire\Valores;
 
+use App\Services\TransacaoService;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class TotaisPeriodo extends Component
 {
+    public $periodo;
+    public $data;
+    public $ano;
+    public $mes;
+    public $dia;
+    public $receita;
+    public $despesa;
+    public $saldo;
+
+    public function mount(){
+        $this->periodo = "mensal";
+        $this->ano = now()->format("Y");
+        $this->mes = now()->format("m");
+        $this->dia = now()->format("d");
+    }
+
     public function render()
     {
+        $this->calcularReceita();
+        $this->calcularDespesa();
+        $this->saldo = $this->receita - $this->despesa;
+        
         return view('livewire.valores.totais-periodo');
+
+    }
+
+    private function calcularReceita(){
+        $this->receita = $this->calcularTotal('receita');
+    }
+
+    private function calcularDespesa(){
+        $this->despesa = $this->calcularTotal('despesa');
+    }
+
+    private function calcularTotal(string $tipo){
+        $transacoes = (new TransacaoService(userId: Auth::user()->id))->resumoTransacoes(tipo: $tipo, ano: $this->ano, mes: $this->mes, dia: $this->dia, periodo: $this->periodo);
+        
+        $total = 0;
+        foreach($transacoes as $transacao){
+            $total += $transacao->total;
+        }
+        return $total;
+    }
+
+    private function montarData(){
     }
 }
