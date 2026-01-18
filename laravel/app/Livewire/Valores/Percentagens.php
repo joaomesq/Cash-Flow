0f265b644c3c71dcb7e66df3ab98a1264c102eb4
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-17 21:08:15 -0300

    create component para calcular a percentagem dos valores

---
+++ laravel/app/Livewire/Valores/Percentagens.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Valores;
+
+use Livewire\Component;
+
+class Percentagens extends Component
+{
+    public function render()
+    {
+        return view('livewire.valores.percentagens');
+    }
+}
