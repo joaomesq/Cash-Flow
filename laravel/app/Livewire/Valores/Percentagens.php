24ad1f8bbcbcaebe000ed3e057dd20521a69bf84
João Mesquita <joaob.mesquita1@gmail.com>
2026-01-18 20:02:47 -0300

    implementação do cálculo da percentaagem de receita e despesa

--- laravel/app/Livewire/Valores/Percentagens.php
+++ laravel/app/Livewire/Valores/Percentagens.php
@@ -2,12 +2,49 @@
 
 namespace App\Livewire\Valores;
 
+use App\Services\TransacaoService;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class Percentagens extends Component
 {
+    public $receita;
+    public $despesa;
+    
+    private $ano, $mes, $dia;
+    private $periodo;
+
+    public function mount(){
+        $this->periodo = 'mensal';
+        $this->ano = now()->format('Y');
+        $this->mes = now()->format('m');
+        $this->dia = now()->format('d');
+    }
+
     public function render()
     {
+        $this->receita = $this->calcularPercentagem('receita');
+        $this->despesa = $this->calcularPercentagem('despesa');
         return view('livewire.valores.percentagens');
+    }
+
+    private function calcularPercentagem(string $tipo){
+        $total = $this->pegarTotal('receita') + $this->pegarTotal('despesa');
+        
+        if(strtolower($tipo) == 'receita'):
+            return ($this->pegarTotal('receita')/$total) * 100;
+        elseif(strtolower($tipo) == 'despesa'):
+            return ($this->pegarTotal('despesa')/$total) * 100;
+        endif;
+    }
+
+    private function pegarTotal(string $tipo){
+        $transacoes = (new TransacaoService(userId: Auth::user()->id))->resumoTransacoes(tipo: strtolower($tipo), ano: $this->ano, mes: $this->mes, dia: $this->dia, periodo: $this->periodo);
+        
+        $total = 0;
+        foreach ($transacoes as $transacao) {
+            $total += $transacao->total;
+        }
+        return $total;
     }
 }
--- laravel/resources/views/livewire/valores/percentagens.blade.php
+++ laravel/resources/views/livewire/valores/percentagens.blade.php
@@ -1,3 +1,5 @@
 <div>
+    <h2>Receita: {{ number_format($receita, 2, ',', '.') }}%</h2>
+    <h2>Despesa: {{ number_format($despesa, 2, ',', '.') }}%</h2>
     {{"If you look to others for fulfillment, you will never truly be fulfilled."}}
 </div>
