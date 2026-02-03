f76ffd9f2f1d5acdb1426bc837aa30205732850b
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-03 23:33:19 +0100

    defindo o visual mobile do register-transacao

--- laravel/resources/views/transacao/register.blade.php
+++ laravel/resources/views/transacao/register.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
     <section class="main p-4">
-        <div class="w-full">
+        <div class="w-full border border-gray-800 rounded shadow py-2">
             <livewire:transacao.resumo-mensal />
         </div>
 
-        <div class="bg-white mt-4">
+        <div class="w-full border border-gray-800 rounded mt-4">
             <livewire:transacao.register-transacao />
         </div>
 
