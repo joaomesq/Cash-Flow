649e5c65c84efa34c55ffe5368cbd714bd40bfa3
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-03 23:08:32 +0100

    defindo o visual desktop para resumo-mensal

--- laravel/resources/views/livewire/transacao/resumo-mensal.blade.php
+++ laravel/resources/views/livewire/transacao/resumo-mensal.blade.php
@@ -1,12 +1,12 @@
 <section>
-    <div class="mb-2 text-gray-300">
-        <div class="data text-end p-2 mb-4">
+    <div class="mb-2 lg:grid lg:grid-cols-[1fr_3fr] items-center gap-2 text-gray-300">
+        <div class="data text-end p-2 mb-4 lg:mb-0">
             <button wire:click="backMonth" class="lg border px-2 rounded w-[50px]"><</button>
-            <label class="text-lg mx-2">{{ $data }}</label>
+            <label class="text-lg font-semibold mx-2">{{ $data }}</label>
             <button class="lg border px-2 rounded w-[50px]" wire:click="nextMonth()">></button>
         </div>
         
-        <div class="grid grid-cols-3 px-2 items-center justify-items-center">
+        <div class="grid grid-cols-3 items-center px-4 lg:justify-items-center">
             <div class="receita">
                 <p class="text-lg">Receita</p>
                 <span class="text-green-600">AO {{ number_format($receita, 2, ',', '.') }}</span>
