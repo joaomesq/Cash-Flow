e3d4e107ddf5312c121899840745b51274303448
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-03 21:13:09 +0100

    Definindo o estilo mobile para resumo-mensal

--- laravel/resources/views/livewire/transacao/resumo-mensal.blade.php
+++ laravel/resources/views/livewire/transacao/resumo-mensal.blade.php
@@ -1,24 +1,26 @@
 <section>
-    <div>
-        <div class="data">
-            <button wire:click="backMonth"><</button>
-            <label>{{ $data }}</label>
-            <button wire:click="nextMonth()">></button>
+    <div class="mb-2 text-gray-300">
+        <div class="data text-end p-2 mb-4">
+            <button wire:click="backMonth" class="lg border px-2 rounded w-[50px]"><</button>
+            <label class="text-lg mx-2">{{ $data }}</label>
+            <button class="lg border px-2 rounded w-[50px]" wire:click="nextMonth()">></button>
         </div>
+        
+        <div class="grid grid-cols-3 px-2 items-center justify-items-center">
+            <div class="receita">
+                <p class="text-lg">Receita</p>
+                <span class="text-green-600">AO {{ number_format($receita, 2, ',', '.') }}</span>
+            </div>
 
-        <div class="receita">
-            <p>Receita</p>
-            <span>AO {{ number_format($receita, 2, ',', '.') }}</span>
-        </div>
+            <div class="despesa">
+                <p class="text-lg">Despesa</p>
+                <span class="text-red-500">AO {{ number_format($despesa, 2, ',', '.') }}</span>
+            </div>
 
-        <div class="despesa">
-            <p>Despesa</p>
-            <span>AO {{ number_format($despesa, 2, ',', '.') }}</span>
-        </div>
-
-        <div class="saldo">
-            <p>Saldo</p>
-            <span>AO {{ number_format($saldo, 2, ',', '.') }}</span>
+            <div class="saldo">
+                <p class="text-lg">Saldo</p>
+                <span class="{{ ($saldo >= 0)? 'text-green-500': 'text-red-500' }}">AO {{ number_format($saldo, 2, ',', '.') }}</span>
+            </div>
         </div>
     </div>
 </section>
