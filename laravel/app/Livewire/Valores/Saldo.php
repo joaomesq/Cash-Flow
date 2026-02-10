9a63305bb3fb83b757f9be08be80dba815f0e5fc
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-11 00:36:27 +0100

    atualizadndo o docker-compose.yml para atribuir os fichieros/components criados ao usuário do sistema linux,retirando o super root(0) para impedir de  pedir permissão toda vez que realizar alguma alteração

--- laravel/app/Livewire/Valores/Saldo.php
+++ laravel/app/Livewire/Valores/Saldo.php
@@ -6,6 +6,10 @@ use Livewire\Component;
 
 class Saldo extends Component
 {
+    public $priodo;
+    public $mes = null;
+    public $ano = null;
+
     public function render()
     {
         return view('livewire.valores.saldo');
