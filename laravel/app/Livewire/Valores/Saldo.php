04dae5e6e0cf1eff146f1144fdd26793254611ec
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-11 00:50:07 +0100

    documentando o component saldo

--- laravel/app/Livewire/Valores/Saldo.php
+++ laravel/app/Livewire/Valores/Saldo.php
@@ -4,11 +4,26 @@ namespace App\Livewire\Valores;
 
 use Livewire\Component;
 
+/**
+ * Componet que calcula o saldo disponivel
+ * 
+ * Este componente calcula o saldo disponivel para determinado periodo[mensal, anual, todo]
+ * @param string $perido = [mensal, anual, todo] default = todo 
+ * @param int $mes = define o mês para o saldo - default = now
+ * @param int $ano = define o ano para o saldo - default = now
+ **/
+
 class Saldo extends Component
 {
-    public $priodo;
-    public $mes = null;
-    public $ano = null;
+    public $periodo;
+    public $mes;
+    public $ano;
+
+    public function mount(){
+        $this->periodo = 'todo';
+        $this->mes = now('m');
+        $this->ano = now('Y');
+    }
 
     public function render()
     {
