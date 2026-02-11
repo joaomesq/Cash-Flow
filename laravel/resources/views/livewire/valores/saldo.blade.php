f923dc55de4d862d3c6fe0d256932610f5ddfbbd
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-11 23:24:38 +0100

    Definindo o estilo do component saldo

--- laravel/resources/views/livewire/valores/saldo.blade.php
+++ laravel/resources/views/livewire/valores/saldo.blade.php
@@ -1,4 +1,6 @@
-<div>
-    <h2 class="text-2xl font-bold">Saldo Atual</h2>
-    <p class="text-2xl font-bold">AO {{ number_format($saldo, 2, ',', '.') }}</p>
+<div class="bg-blue-600 dark:bg-transparent dark:border-blue-600 dark:border dark:text-gray-300 p-4 text-white rounded">
+    <h2 class="font-light mb-2">Saldo Actual</h2>
+    <p class="text-xl font-bold">AO {{ number_format($saldo, 2, ',', '.') }}</p>
+    <span class="border-t border-white/50 w-full block mt-4"></span>
+    <p class="mt-2 text-sm font-light ">Conta corrente</p>
 </div>
