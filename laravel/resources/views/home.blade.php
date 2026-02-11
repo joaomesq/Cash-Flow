6ec8e975edbca173f5fdf079ed9fe2c7bacabe90
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-11 23:24:38 +0100

    Definindo o estilo do component saldo

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
-    <section class="valores container mx-auto">
+    <section class="px-4 valores container mx-auto grid grid-cols-3 gap-4">
         <div class="saldo">
             <livewire:valores.saldo />
         </div>
+        <div>a</div>
+        <div>a</div>
     </section>
 </x-app-layout>
