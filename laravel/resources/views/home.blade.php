15d44ab69399eea6133d7385ab97b8d3dcc0460c
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-12 00:25:43 +0100

    melhorando o estilo de saldo

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <section class="dark:text-gray-300 text-white px-4 valores container mx-auto grid grid-cols-3 gap-4">
-        <div class="saldo">
+        <div class="saldo bg-blue-600 dark:bg-transparent dark:border-blue-600 dark:border dark:text-gray-300 p-4 text-white rounded-lg">
             <livewire:valores.saldo />
+            <span class="border-t border-white/50 w-full block mt-4"></span>
+            <p class="mt-2 text-sm font-light ">Conta corrente</p>
         </div>
 
         <div class="rounded-lg px-4 py-4 receita bg-green-600 dark:bg-transparent dark:border-green-600 dark:border ">
--- laravel/resources/views/livewire/valores/saldo.blade.php
+++ laravel/resources/views/livewire/valores/saldo.blade.php
@@ -1,6 +1,4 @@
-<div class="bg-blue-600 dark:bg-transparent dark:border-blue-600 dark:border dark:text-gray-300 p-4 text-white rounded">
+<div>
     <h2 class="font-light mb-2">Saldo Actual</h2>
     <p class="text-xl font-bold">AO {{ number_format($saldo, 2, ',', '.') }}</p>
-    <span class="border-t border-white/50 w-full block mt-4"></span>
-    <p class="mt-2 text-sm font-light ">Conta corrente</p>
 </div>
