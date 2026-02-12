be9c8458503352318033ab040a5b045529bcfa5c
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-13 00:09:17 +0100

    create component grafico/fluxo-caixa

---
+++ laravel/app/Livewire/Graficos/FluxoCaixa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Graficos;
+
+use Livewire\Component;
+
+class FluxoCaixa extends Component
+{
+    public function render()
+    {
+        return view('livewire.graficos.fluxo-caixa');
+    }
+}
