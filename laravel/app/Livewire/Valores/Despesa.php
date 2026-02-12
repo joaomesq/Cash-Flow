88b4adf8470c84e91571a0444c486c8d0ee9ff01
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-12 20:19:02 +0100

    Create component-Despesa

---
+++ laravel/app/Livewire/Valores/Despesa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Valores;
+
+use Livewire\Component;
+
+class Despesa extends Component
+{
+    public function render()
+    {
+        return view('livewire.valores.despesa');
+    }
+}
