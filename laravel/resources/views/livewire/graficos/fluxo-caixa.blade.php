81e6836610c9fe9c09a351c61f0a5f4f3da94c4a
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-13 23:39:42 +0100

    Definição doformato da label e preenchimento das labels e separação dos dados por tipo

--- laravel/resources/views/livewire/graficos/fluxo-caixa.blade.php
+++ laravel/resources/views/livewire/graficos/fluxo-caixa.blade.php
@@ -1,4 +1,7 @@
 <div class="p-2 overflow-x-auto  bg-white dark:bg-transparent border rounded-lg shadow-lg dark:border-gray-400">
     <h2 class="font-semibold text-lg dark:text-gray-300">Fluxo de Caixa</h2>
-    {{-- To attain knowledge, add things every day; To attain wisdom, subtract things every day. --}}
+    
+    @foreach ($dados as $item)
+        <p>{{ $item }}</p>
+    @endforeach
 </div>
