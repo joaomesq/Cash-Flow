bbcf090b270c8c583ab6accad14129d100b0092a
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-18 23:36:29 +0100

    ajuste do visual do component resumo-mensal

--- laravel/resources/views/livewire/transacao/resumo-mensal.blade.php
+++ laravel/resources/views/livewire/transacao/resumo-mensal.blade.php
@@ -1,24 +1,24 @@
-<section>
+<section class="dark:border dark:border-gray-600 bg-white dark:bg-transparent rounded-lg shadow p-4 w-full dark:text-gray-300">
     <div class="mb-2 lg:grid lg:grid-cols-[1fr_3fr] items-center gap-2">
         <div class="data text-end p-2 mb-4 lg:mb-0">
-            <button wire:click="backMonth" class="lg border px-2 rounded w-[50px]"><</button>
-            <label class="text-lg font-semibold mx-2">{{ $data }}</label>
-            <button class="lg border px-2 rounded w-[50px]" wire:click="nextMonth()">></button>
+            <button wire:click="backMonth" class="lg border dark:border-gray-600 rounded w-[50px] h-[30px]"><</button>
+            <label class="text-lg mx-2">{{ $data }}</label>
+            <button class="lg border rounded w-[50px] dark:border-gray-600 h-[30px]" wire:click="nextMonth()">></button>
         </div>
         
         <div class="grid grid-cols-3 items-center px-4 lg:justify-items-center">
             <div class="receita">
-                <p class="text-lg">Receita</p>
+                <p class="">Receita</p>
                 <span class="text-green-600">AO {{ number_format($receita, 2, ',', '.') }}</span>
             </div>
 
             <div class="despesa">
-                <p class="text-lg">Despesa</p>
+                <p class="">Despesa</p>
                 <span class="text-red-500">AO {{ number_format($despesa, 2, ',', '.') }}</span>
             </div>
 
             <div class="saldo">
-                <p class="text-lg">Saldo</p>
+                <p class="">Saldo</p>
                 <span class="{{ ($saldo >= 0)? 'text-green-500': 'text-red-500' }}">AO {{ number_format($saldo, 2, ',', '.') }}</span>
             </div>
         </div>
--- laravel/resources/views/transacao/register.blade.php
+++ laravel/resources/views/transacao/register.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <section class="main p-4">
-        <div class="w-full shadow rounded-lg bg-white dark:text-gray-300 p-4 md:py-6 mx-auto container mb-4">
+        <div class="w-full container mb-4">
             <livewire:transacao.resumo-mensal />
         </div>
 
