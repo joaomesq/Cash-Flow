4d1ea3fc238e6fab850bc94e30bb640016dd3fdf
João Mesquita <joaob.mesquita1@gmail.com>
2026-02-22 09:09:48 +0100

    Sumindo com a tab-bar em telas maiores

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -34,7 +34,7 @@
             </main>
 
             <footer class="bg-gray-100 dark:bg-gray-900 pt-1">
-                    <section class="tab-bar dark:text-gray-300 text-gray-500 mt-8 fixed bottom-0 left-0 right-0 bg-white mt-6 p-4 shadow dark:bg-gray-800">
+                    <section class="tab-bar dark:text-gray-300 lg:hidden text-gray-500 mt-8 fixed bottom-0 left-0 right-0 bg-white mt-6 p-4 shadow dark:bg-gray-800">
                         <nav>
                             <ul class="grid text-center font-bold grid-cols-4 gap-4 w-full container mx-auto uppercase">
                                 <li>
