3799df7d5faeb30fedf99597062905764a2946c9
João Mesquita <joaob.mesquita1@gmail.com>
2026-03-07 08:13:41 +0100

    ajustando a apresentação de receita, despesa e saldo no histórico

--- resources/views/transacao/historico.blade.php
+++ resources/views/transacao/historico.blade.php
@@ -1,12 +1,13 @@
 <x-app-layout>
     <section class="main p-4">
-        <div class="dark:text-gray-300 text-white mb-4 valores mx-auto grid grid-cols-3 gap-4">
+        <div class="dark:text-gray-300 text-white mb-4 valores mx-auto grid lg:grid-cols-2 gap-4">
             <article class="saldo bg-blue-600 dark:bg-transparent dark:border-blue-600 dark:border dark:text-gray-300 p-4 text-white rounded-lg">
                 <livewire:valores.saldo periodo="todo" />
                 <span class="border-t border-white/50 w-full block mt-4"></span>
                 <p class="mt-2 text-sm font-light ">Conta corrente</p>
             </article>
-
+            
+            <div class="grid grid-cols-2 gap-4">
             <article class="receita p-4 bg-green-600 dark:bg-transparent dark:border-green-600 dark:border rounded-lg">
                 <livewire:valores.receita periodo="todo"/>
                 <span class="px-4 border-t border-white/50 w-full block mt-4"></span>
@@ -18,6 +19,7 @@
                 <span class="px-4 border-t border-white/50 w-full block mt-4"></span>
                 <p class="mt-2 text-sm font-light ">Tudo</p>
             </article>
+            </div>
         </div>
 
         <div class="lista">
