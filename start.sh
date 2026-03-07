b85d4e541ad377d5f2f201cdeb72f11b7299de22
João Mesquita <joaob.mesquita1@gmail.com>
2026-03-07 23:05:47 +0100

    update start.sh

--- start.sh
+++ start.sh
@@ -6,6 +6,12 @@ echo "🚀 Iniciando aplicação Laravel no Render..."
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 chmod -R 775 /var/www/storage /var/www/bootstrap/cache
 
+#!/bin/sh
+
+# Ajusta dono e permissões
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
 # Limpando caches do Laravel
 php artisan config:clear
 php artisan route:clear
