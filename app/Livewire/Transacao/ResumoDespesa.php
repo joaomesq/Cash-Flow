0c0d20137213099dc8e5f0460ab0968f7581b1d6
João Mesquita <joaob.mesquita1@gmail.com>
2026-03-15 00:57:32 +0100

    create component ResumoDespesa

---
+++ app/Livewire/Transacao/ResumoDespesa.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Transacao;
+
+use Livewire\Component;
+
+class ResumoDespesa extends Component
+{
+    public function render()
+    {
+        return view('livewire.transacao.resumo-despesa');
+    }
+}
