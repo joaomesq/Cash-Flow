848749ade5bac3f66bdeddb48bf7d1ca2f96d75a
João Mesquita <joaob.mesquita1@gmail.com>
2026-03-15 00:04:11 +0100

    montando lógica para pegar receitas

--- app/Livewire/Transacao/ResumoReceita.php
+++ app/Livewire/Transacao/ResumoReceita.php
@@ -2,12 +2,20 @@
 
 namespace App\Livewire\Transacao;
 
+use App\Services\TransacaoService;
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 
 class ResumoReceita extends Component
 {
+    public $receitas;
+    
     public function render()
-    {
+    {   $this->setReceitas(new TransacaoService(userId: Auth::user()->id));
         return view('livewire.transacao.resumo-receita');
+    }
+
+    public function setReceitas(TransacaoService $transacaoService){
+        $this->receitas = $transacaoService->resumoTransacoes(tipo: 'receita', coluna: "categoria", periodo: 'todo');
     }
 }
